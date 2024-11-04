2f3c30213c9257749e1c97fbb3a94d5e04474c20
Kristina Budnik <ssspers92@gmail.com>
2024-11-04 18:53:19 +0300

    Created method update_question_count

--- study/models.py
+++ study/models.py
@@ -1,10 +1,14 @@
 from django.db import models
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+
 
 class Tests(models.Model):
     name_of_test = models.CharField(
         max_length=300, verbose_name='Название теста', unique=True,
         help_text='Название теста'
     )
+    is_active = models.BooleanField(verbose_name='Активен', default=True)
 
     def __str__(self):
         return self.name_of_test
@@ -15,15 +19,17 @@ class Tests(models.Model):
 
 class Questions(models.Model):
     name_of_question = models.TextField(verbose_name='Вопрос', unique=True, help_text='Введите вопро ')
-    test = models.ForeignKey(Tests, on_delete=models.SET_NULL, related_name='test')
+    test = models.ForeignKey(Tests, on_delete=models.CASCADE, related_name='questions')
+    question_score = models.PositiveIntegerField(verbose_name='Количество баллов', default=0)
 
     def __str__(self):
         return self.name_of_question
 
 class ChoicesForQuestions(models.Model):
     name_of_choice = models.TextField(verbose_name='Вариант ответа', unique=True, help_text='Введите вариант ответа')
-    question = models.ForeignKey(Questions, on_delete=models.SET_NULL, related_name='answer_choice')
+    question = models.ForeignKey(Questions, on_delete=models.CASCADE, related_name='choices')
     right_answer = models.BooleanField(verbose_name='Правильный ответ', default=False)
+    score = models.PositiveIntegerField(verbose_name='Балл', default=0)
 
     def __str__(self):
         return self.name_of_choice
@@ -34,8 +40,9 @@ class ChoicesForQuestions(models.Model):
 
 class TextAnswerForQuestions(models.Model):
     answer = models.TextField(verbose_name='Ответ', help_text='Ваш ответ')
-    question = models.ForeignKey(Questions, on_delete=models.SET_NULL, related_name='answer_text')
+    question = models.ForeignKey(Questions, on_delete=models.CASCADE, related_name='text_answers')
     right_answer = models.BooleanField(verbose_name='Правильный ответ', default=False)
+    score = models.PositiveIntegerField(verbose_name='Балл', default=0)
 
     def __str__(self):
         return self.answer
@@ -43,3 +50,29 @@ class TextAnswerForQuestions(models.Model):
     class Meta:
         verbose_name = 'Ответ на вопрос'
         verbose_name_plural = 'Ответы на вопросы'
+
+class UserTestAssignment(models.Model):
+
+    CHOICES_OF_STATUS = (
+        ('in_progress', 'В процессе'),
+        ('passed', 'Пройден'),
+        ('failed', 'Не пройден'),
+    )
+
+    user = models.ForeignKey(
+        'users.User', on_delete=models.CASCADE, related_name='user_assignments',
+        null=True, blank=True, verbose_name='Пользователь'
+    )
+    users_test = models.ForeignKey('study.Tests', on_delete=models.CASCADE, related_name='assigned_tests', verbose_name='Тест')
+    attempts = models.PositiveIntegerField(verbose_name='Количество попыток', default=0)
+    attempt_at = models.DateTimeField(verbose_name='Попытка', auto_now_add=True)
+    status = models.CharField(max_length=20,
+        choices=CHOICES_OF_STATUS,
+        default='in_progress', verbose_name='Статус'
+    )
+
+@receiver([post_save, post_delete], sender=Questions)
+def update_question_count(self, instance, **kwargs):
+    test = instance.test
+    test.question_count = test.question_count.count()
+    test.save()
