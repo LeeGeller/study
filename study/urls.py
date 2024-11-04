91fa87a8306da045740dec37a6e37b9d9fd03ccc
Kristina Budnik <ssspers92@gmail.com>
2024-11-04 19:32:08 +0300

    Created url for home page

--- study/urls.py
+++ study/urls.py
@@ -1,6 +1,10 @@
+from django.urls import path
+
 from study.apps import StudyConfig
+from study.views import HomeListView
 
 appname = StudyConfig.name
 urlpatterns = [
+    path('', HomeListView.as_view(template_name='study/index/html'), name='home'),
 
 ]
