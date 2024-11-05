36f7f5731e9a7e8a73c479e4cdd3923a738ce224
Kristina Budnik <ssspers92@gmail.com>
2024-11-05 15:42:16 +0300

    added new app core

---
+++ core/mixins/permissions.py
@@ -0,0 +1,18 @@
+from audioop import reverse
+
+from django.core.exceptions import PermissionDenied
+from django.shortcuts import redirect
+
+
+class UserIsAuthorizedMixin:
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return redirect(reverse('users:login'))
+        return super().dispatch(request, *args, **kwargs)
+
+
+class UserIsSupervisorMixin:
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated or not request.user.groups.filter('Supervisor').exists():
+            raise PermissionDenied("You do not have permission to access this page.")
+        return super().dispatch(request, *args, **kwargs)
