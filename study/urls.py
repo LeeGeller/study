83d52f4c163c7c7b572474daa009ef56dec14406
Kristina Budnik <ssspers92@gmail.com>
2024-11-05 15:43:36 +0300

    added new path for list of tests

--- study/urls.py
+++ study/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from study.apps import StudyConfig
-from study.views import HomeListView
+from study.views import HomeListView, TestsListView
 
 appname = StudyConfig.name
 urlpatterns = [
     path('', HomeListView.as_view(template_name='study/home.html'), name='home'),
+    path('tests/', TestsListView.as_view(), name='tests')
 
 ]
