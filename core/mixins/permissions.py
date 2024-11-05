747a902c275acffc2c9e5465f0c2509a737b61fb
Kristina Budnik <ssspers92@gmail.com>
2024-11-05 16:02:55 +0300

    Fixed name in groups

--- core/mixins/permissions.py
+++ core/mixins/permissions.py
@@ -13,6 +13,6 @@ class UserIsAuthorizedMixin:
 
 class UserIsSupervisorMixin:
     def dispatch(self, request, *args, **kwargs):
-        if not request.user.is_authenticated or not request.user.groups.filter('Supervisor').exists():
+        if not request.user.is_authenticated or not request.user.groups.filter(name='Supervisor').exists():
             raise PermissionDenied("You do not have permission to access this page.")
         return super().dispatch(request, *args, **kwargs)
