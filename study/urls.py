51e0a4105a86895c1938197cefefc5307a73dab0
Kristina Budnik <ssspers92@gmail.com>
2024-11-06 16:58:32 +0300

    Added url for TestsListView

--- study/urls.py
+++ study/urls.py
@@ -6,6 +6,6 @@ from study.views import HomeListView, TestsListView
 appname = StudyConfig.name
 urlpatterns = [
     path('', HomeListView.as_view(template_name='study/home.html'), name='home'),
-    path('tests/', TestsListView.as_view(), name='tests')
+    path('tests/', TestsListView.as_view(), name='tests_list')
 
 ]
