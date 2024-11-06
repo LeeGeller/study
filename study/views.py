aad09e7d76b3e548b86555b926ff9d5b3463d2bc
Kristina Budnik <ssspers92@gmail.com>
2024-11-06 17:00:58 +0300

    Created TestsListView

--- study/views.py
+++ study/views.py
@@ -6,5 +6,10 @@ from study.models import Tests
 class HomeListView(LoginRequiredMixin, ListView):
     model = Tests
 
+
+class TestsListView(LoginRequiredMixin, ListView):
+    model = Tests
+    login_url = 'login'
+
     def get_queryset(self):
         return Tests.objects.filter(is_active=True)
