544931c349406489be24eddf19a3fe7f48a75143
Kristina Budnik <ssspers92@gmail.com>
2024-11-06 16:58:05 +0300

    Added re_path

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.static import static
 from django.contrib.auth.views import LoginView, LogoutView
-from django.urls import path
+from django.urls import path, re_path
+from django.views.generic import RedirectView
 
 from studyplatform import settings
 from users.apps import UsersConfig
@@ -8,7 +9,8 @@ from users.apps import UsersConfig
 appname = UsersConfig.name
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout')
+    path('logout/', LogoutView.as_view(), name='logout'),
+    re_path(r'^accounts/login/$', RedirectView.as_view(url='/login/', permanent=False)),
 ]
 
 if settings.DEBUG:
