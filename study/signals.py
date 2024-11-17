25d1a6fd7ecd93555d9cc9f89814d280e1a06936
Kristina Budnik <ssspers92@gmail.com>
2024-11-17 18:50:34 +0300

    Created signal for update_tests_score

--- study/signals.py
+++ study/signals.py
@@ -1,7 +1,7 @@
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 
-from study.models import Questions
+from study.models import Questions, Tests, ChoicesForQuestions
 
 
 @receiver([post_save, post_delete], sender=Questions)
@@ -9,3 +9,8 @@ def update_questions_count(self, instance, **kwargs):
     test = instance.test
     test.questions_count = test.question_count.count()
     test.save()
+
+receiver([post_save, post_delete], sender=Tests)
+def update_tests_score(self, instance, **kwargs):
+    question = instance
+    choices_score = ChoicesForQuestions.objects.filter()
