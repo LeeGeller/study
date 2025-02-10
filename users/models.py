aa9910b8677e6ae518410380004f1c26ea484944
Kris <ssspers92@gmail.com>
2025-02-10 15:52:25 +0300

    Created User list for admin. Added fields: role and date_of_birth.
    Added to users list for html info about user's role and birthday

--- users/models.py
+++ users/models.py
@@ -3,10 +3,12 @@ from django.db import models
 
 
 class User(AbstractUser):
-    role = models.CharField(max_length=300, verbose_name='Роль', blank=True, null=True)
+    role = models.CharField(max_length=300, verbose_name='Роль')
+    date_of_birth = models.DateField(verbose_name='Дата рождения', blank=True, null=True)
 
     def __str__(self):
         return self.username
+
 
     class Meta:
         verbose_name = 'user'
