3ceb3259353fb6c5a40495f8f4baa2939fa77bc5
Kris <ssspers92@gmail.com>
2025-02-10 18:45:24 +0300

    Created UsersCreateView in views.py

--- users/views.py
+++ users/views.py
@@ -1,8 +1,11 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView
 
+from users.forms import UserCreationForm
 from users.models import User
+from users.utils import generate_random_password
 
 
 class UsersListView(LoginRequiredMixin, ListView):
@@ -15,3 +18,17 @@ class UsersListView(LoginRequiredMixin, ListView):
         context = super().get_context_data(**kwargs)
         context['users'] = context['object_list']
         return context
+
+
+class UsersCreateView(LoginRequiredMixin, CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('users:users')
+
+    def form_valid(self, form):
+        user = form.save(commit=False)
+        random_password = generate_random_password()
+        user.set_password(random_password)
+        user.save()
+
+        return super().form_valid(form)
