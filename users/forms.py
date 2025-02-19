9e9b73fdb23611fe856ccb78b50f98abf390e2e8
Kris <ssspers92@gmail.com>
2025-02-19 19:28:44 +0300

    Added widget to field role

--- users/forms.py
+++ users/forms.py
@@ -5,12 +5,28 @@ from users.models import User
 
 
 class UserCreationForm(forms.ModelForm):
-    role = forms.ModelChoiceField(queryset=Group.objects.all(), label='Роль', empty_label='Выберите роль',
-                                  required=True)
+    role = forms.ModelChoiceField(
+        queryset=Group.objects.all(),
+        label='Роль',
+        empty_label='Выберите роль',
+        required=True, widget=forms.Select()
+    )
 
-    password = forms.CharField(widget=forms.TextInput(attrs={'readonly': 'readonly', 'id': 'id_password'}),
-                               required=False, label='Пароль')
+    password = forms.CharField(
+        widget=forms.TextInput(attrs={'readonly': 'readonly', 'id': 'id_password'}),
+        required=False, label='Пароль')
 
     class Meta:
         model = User
         fields = ('username', 'date_of_birth', 'role', 'password')
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        group = self.cleaned_data.get('role')
+
+        if commit:
+            user.save()
+            user.groups.clear()
+            user.groups.set([group])
+
+        return user
