f145b7911e1effbf47b4d96f972c5ae800a97194
Kris <ssspers92@gmail.com>
2025-02-19 18:33:20 +0300

    Fields for password readonly and user can choose group for role

--- users/forms.py
+++ users/forms.py
@@ -1,8 +1,16 @@
 from django import forms
+from django.contrib.auth.models import Group
+
 from users.models import User
 
 
 class UserCreationForm(forms.ModelForm):
+    role = forms.ModelChoiceField(queryset=Group.objects.all(), label='Роль', empty_label='Выберите роль',
+                                  required=True)
+
+    password = forms.CharField(widget=forms.TextInput(attrs={'readonly': 'readonly', 'id': 'id_password'}),
+                               required=False, label='Пароль')
+
     class Meta:
         model = User
-        fields = ('username', 'date_of_birth', 'role')
+        fields = ('username', 'date_of_birth', 'role', 'password')
